2070edbcaadfbbf30672947414089ec6deb43c2d
w1y0n0 <wiyono30@gmail.com>
2023-06-30 16:20:45 +0700

    raw sql and query builder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    // fetch all users
+    // $users = DB::select("select * from users");
+    // $users = DB::table('users')->where('id', 1)->get();
+    $users = DB::table('users')->get();
+
+    // create new user
+    // $user = DB::insert('insert into users (name,email,password) values(?,?,?)', [
+    //     'LabSI',
+    //     'labsipnc@gmail.com',
+    //     'password',
+    // ]);
+    // $user = DB::table('users')->insert([
+    //     'name' => 'Labsi',
+    //     'email' => 'labsi@gmail.com',
+    //     'password' => 'password',
+    // ]);
+
+    // update user
+    // $user = DB::update("update users set email=? where id=?", [
+    //     'labsi@gmail.com',
+    //     2,
+    // ]);
+    // $user = DB::table('users')->where('id', 4)->update(['email' => 'abc@gmail.com']);
+
+    // delete user
+    // $user = DB::delete('delete from users where id=2');
+    // $user = DB::table('users')->where('id', 4)->delete();
+    dd($users);
 });
 
 Route::get('/dashboard', function () {
@@ -28,4 +57,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
